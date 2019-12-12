870864ad354a380be92a9bbf632aa3e30b32e59c
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-12 03:02:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,13 +13,3 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
  
-def speak_to_grandma(phrase)
-  if phrase == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
-  end
-  if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-  else
-    return "NO, NOT SINCE 1938!"
-  end
-end
